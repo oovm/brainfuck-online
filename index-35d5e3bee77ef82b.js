e53161465fda126de9066d11f705d2256b0a327f
GalAster <galaster@foxmail.com>
2020-10-18 08:12:27 +0000

    Deploying to gh-pages from  @ 6a5ac1ff03a44cc0c8dcd2f35f698290d21c0b1f 🚀

--- index-35d5e3bee77ef82b.js
+++ index-35d5e3bee77ef82b.js
@@ -466,8 +466,8 @@ async function init(input) {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper243 = function(arg0, arg1, arg2) {
-        var ret = makeMutClosure(arg0, arg1, 80, __wbg_adapter_16);
+    imports.wbg.__wbindgen_closure_wrapper304 = function(arg0, arg1, arg2) {
+        var ret = makeMutClosure(arg0, arg1, 126, __wbg_adapter_16);
         return addHeapObject(ret);
     };
 
